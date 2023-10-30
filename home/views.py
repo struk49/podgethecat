dc0863852a669d7320cb61a50b876aa127e06ac3
struk49 <andrewstruk@yahoo.co.uk>
2023-10-30 12:22:21 +0000

    added images category and items to front page

--- home/views.py
+++ home/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 
+from item.models import Category, Item
+
 # Create your views here.
 
 def index(request):
-    return render(request, "home/index.html")
+    items = Item.objects.filter(is_sold=False)[0:6]
+    categories = Category.objects.all()
+    return render(request, "home/index.html", {
+        'categories': categories,
+        'items': items,
+    })
 
 
 def contacts(request):
