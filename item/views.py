5e4ead3eb9fd022e538747d492cd26773d8700f6
struk49 <andrewstruk@yahoo.co.uk>
2023-11-02 13:38:43 +0000

    added delete function to my items

--- item/views.py
+++ item/views.py
@@ -35,3 +35,11 @@ def new(request):
         'title': 'Add Item',
     })
 
+
+@login_required
+def delete(request, pk):
+    item = get_object_or_404(Item, pk=pk, created_by = request.user)
+    item.delete()
+
+
+    return redirect('dashboard:index' )
